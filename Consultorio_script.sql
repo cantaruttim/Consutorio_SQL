36e411a5fc64fc4a0cd9d6aad360812bc6463942
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-24 21:05:51 -0300

    Update Consultorio_script.sql

--- Consultorio_script.sql
+++ Consultorio_script.sql
@@ -5,17 +5,15 @@ USE CONSULTORIO;
 -- Criando as tabelas
 DROP TABLE IF EXISTS MEDICO;
 CREATE TABLE MEDICO(
-	-- IDMEDICO INT,
     NOME_MEDICO VARCHAR(30) NOT NULL,
     CRM_MEDICO VARCHAR(6) UNIQUE NOT NULL,
     ESPECIALIDADE ENUM('Geral', 'Onco', 'Imuno', 'Orto', 'Endo', 'Psi', 'Neuro', 'Cardio'), 
     TELEFONE_MEDICO VARCHAR(10) NOT NULL
-    );
+);
     
 
 DROP TABLE IF EXISTS PACIENTE;
 CREATE TABLE PACIENTE(
-	-- IDPACIENTE INT,
     NOME_PACIENTE VARCHAR(50) NOT NULL,
     SEXO ENUM('M','F') NOT NULL,
     TELEFONE_PACIENTE VARCHAR(10) NOT NULL,
@@ -25,14 +23,15 @@ CREATE TABLE PACIENTE(
 
 DROP TABLE IF EXISTS CONSULTA;
 CREATE TABLE CONSULTA(
-	-- INCONSULTA INT,
 	IDPACIENTE INT,
     INMEDICO INT
 );
 
+/*
 DROP TABLE MEDICO;
 DROP TABLE PACIENTE;
 DROP TABLE CONSULTA;
+*/
 
 SHOW TABLES;
 
@@ -50,10 +49,67 @@ ALTER TABLE CONSULTA ADD COLUMN IDCONSULTA  INT FIRST;
 ALTER TABLE CONSULTA ADD PRIMARY KEY(IDCONSULTA);
 
 
+DESC MEDICO;
+DESC PACIENTE;
+DESC CONSULTA;
+
+/*
+
+
+**** Observe como as tabelas ficaram após realizarmos as alterações das tabelas assim como
+adicionamos as colunas idmedico, idpaciente, idconsulta como as primeiras consultas de cada tabela assim como
+alteramos o tipo dela para INT e colocamos elas como PRIMARY KEY
+
+
+mysql> SHOW TABLES;
++-----------------------+
+| Tables_in_consultorio |
++-----------------------+
+| consulta              |
+| medico                |
+| paciente              |
++-----------------------+
+3 rows in set (0.00 sec)
+
+mysql> DESC MEDICO;
++-----------------+-------------------------------------------------------------------+------+-----+---------+-------+
+| Field           | Type                                                              | Null | Key | Default | Extra |
++-----------------+-------------------------------------------------------------------+------+-----+---------+-------+
+| IDMEDICO        | int                                                               | NO   | PRI | NULL    |       |
+| NOME_MEDICO     | varchar(30)                                                       | NO   |     | NULL    |       |
+| CRM_MEDICO      | varchar(6)                                                        | NO   | UNI | NULL    |       |
+| ESPECIALIDADE   | enum('Geral','Onco','Imuno','Orto','Endo','Psi','Neuro','Cardio') | YES  |     | NULL    |       |
+| TELEFONE_MEDICO | varchar(10)                                                       | NO   |     | NULL    |       |
++-----------------+-------------------------------------------------------------------+------+-----+---------+-------+
+5 rows in set (0.00 sec)
 
 
 
+mysql> DESC PACIENTE;
++-------------------+---------------+------+-----+---------+-------+
+| Field             | Type          | Null | Key | Default | Extra |
++-------------------+---------------+------+-----+---------+-------+
+| IDPACIENTE        | int           | NO   | PRI | NULL    |       |
+| NOME_PACIENTE     | varchar(50)   | NO   |     | NULL    |       |
+| SEXO              | enum('M','F') | NO   |     | NULL    |       |
+| TELEFONE_PACIENTE | varchar(10)   | NO   |     | NULL    |       |
+| ENDERECO_PACIENTE | varchar(100)  | NO   |     | NULL    |       |
++-------------------+---------------+------+-----+---------+-------+
+5 rows in set (0.00 sec)
 
+
+DESC CONSULTA;
+
++------------+------+------+-----+---------+-------+
+| Field      | Type | Null | Key | Default | Extra |
++------------+------+------+-----+---------+-------+
+| IDCONSULTA | int  | NO   | PRI | NULL    |       |
+| IDPACIENTE | int  | YES  |     | NULL    |       |
+| INMEDICO   | int  | YES  |     | NULL    |       |
++------------+------+------+-----+---------+-------+
+3 rows in set (0.00 sec)
+
+*/
 
 
 
