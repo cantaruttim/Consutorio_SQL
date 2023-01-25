8c2f2d3e1b564da933084a5ce94dd8b8dc28df95
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-24 22:55:49 -0300

    Create backup_consultorio.sql

---
+++ backup_consultorio.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE BKP_CONSULTORIO;
+USE BKP_CONSULTORIO;
+
+/*
++-------------------+---------------+------+-----+---------+-------+
+| Field             | Type          | Null | Key | Default | Extra |
++-------------------+---------------+------+-----+---------+-------+
+| IDPACIENTE        | int           | NO   | PRI | NULL    |       |
+| NOME_PACIENTE     | varchar(50)   | NO   |     | NULL    |       |
+| SEXO              | enum('M','F') | NO   |     | NULL    |       |
+| TELEFONE_PACIENTE | varchar(10)   | NO   |     | NULL    |       |
+| ENDERECO_PACIENTE | varchar(100)  | NO   |     | NULL    |       |
++-------------------+---------------+------+-----+---------+-------+
+
++------------+------+------+-----+---------+-------+
+| Field      | Type | Null | Key | Default | Extra |
++------------+------+------+-----+---------+-------+
+| IDCONSULTA | int  | NO   | PRI | NULL    |       |
+| IDPACIENTE | int  | YES  |     | NULL    |       |
+| INMEDICO   | int  | YES  |     | NULL    |       |
++------------+------+------+-----+---------+-------+
+3 rows in set (0.00 sec)
+
+*/
+
+DROP TABLE IF EXISTS BKP_PACIENTE;
+CREATE TABLE BKP_PACIENTE(
+	IDBKUP INT PRIMARY KEY AUTO_INCREMENT,
+    IDCONSULTA INT,
+	IDPACIENTE INT,
+    NOME_PACIENTE VARCHAR(50),
+    SEXO ENUM('M','F'),
+    TELEFONE_PACIENTE VARCHAR(10),
+    ENDERECO_PACIENTE VARCHAR(100) 
+);
