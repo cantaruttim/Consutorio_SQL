cedf6e29a93510b70f790c0afa15946dbaf8b44e
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-24 21:51:12 -0300

    Update Consultorio_script.sql

--- Consultorio_script.sql
+++ Consultorio_script.sql
@@ -5,10 +5,11 @@ USE CONSULTORIO;
 -- Criando as tabelas
 DROP TABLE IF EXISTS MEDICO;
 CREATE TABLE MEDICO(
-    NOME_MEDICO VARCHAR(30) NOT NULL,
-    CRM_MEDICO VARCHAR(6) UNIQUE NOT NULL,
+    MEDICO VARCHAR(30) NOT NULL,
+    SEXO CHAR(1),
+    CRM VARCHAR(6) UNIQUE NOT NULL,
     ESPECIALIDADE ENUM('Geral', 'Onco', 'Imuno', 'Orto', 'Endo', 'Psi', 'Neuro', 'Cardio'), 
-    TELEFONE_MEDICO VARCHAR(10) NOT NULL
+    TELEFONE_MEDICO VARCHAR(10)
 );
     
 
@@ -24,7 +25,7 @@ CREATE TABLE PACIENTE(
 DROP TABLE IF EXISTS CONSULTA;
 CREATE TABLE CONSULTA(
 	IDPACIENTE INT,
-    INMEDICO INT
+    IDMEDICO INT
 );
 
 /*
@@ -39,13 +40,13 @@ SHOW TABLES;
 -- ALTER TABLES
 -- ADDING PK
 
-ALTER TABLE MEDICO ADD COLUMN IDMEDICO  INT FIRST;
+ALTER TABLE MEDICO ADD COLUMN IDMEDICO  INT NOT NULL FIRST;
 ALTER TABLE MEDICO ADD PRIMARY KEY(IDMEDICO);
 
-ALTER TABLE PACIENTE ADD COLUMN IDPACIENTE  INT FIRST;
+ALTER TABLE PACIENTE ADD COLUMN IDPACIENTE  INT NOT NULL FIRST;
 ALTER TABLE PACIENTE ADD PRIMARY KEY(IDPACIENTE);
 
-ALTER TABLE CONSULTA ADD COLUMN IDCONSULTA  INT FIRST;
+ALTER TABLE CONSULTA ADD COLUMN IDCONSULTA  INT NOT NULL  FIRST;
 ALTER TABLE CONSULTA ADD PRIMARY KEY(IDCONSULTA);
 
 
@@ -74,12 +75,13 @@ mysql> DESC MEDICO;
 | Field           | Type                                                              | Null | Key | Default | Extra |
 +-----------------+-------------------------------------------------------------------+------+-----+---------+-------+
 | IDMEDICO        | int                                                               | NO   | PRI | NULL    |       |
-| NOME_MEDICO     | varchar(30)                                                       | NO   |     | NULL    |       |
-| CRM_MEDICO      | varchar(6)                                                        | NO   | UNI | NULL    |       |
+| MEDICO          | varchar(30)                                                       | NO   |     | NULL    |       |
+| SEXO            | char(1)                                                           | YES  |     | NULL    |       |
+| CRM             | varchar(6)                                                        | NO   | UNI | NULL    |       |
 | ESPECIALIDADE   | enum('Geral','Onco','Imuno','Orto','Endo','Psi','Neuro','Cardio') | YES  |     | NULL    |       |
-| TELEFONE_MEDICO | varchar(10)                                                       | NO   |     | NULL    |       |
+| TELEFONE_MEDICO | varchar(10)                                                       | YES  |     | NULL    |       |
 +-----------------+-------------------------------------------------------------------+------+-----+---------+-------+
-5 rows in set (0.00 sec)
+6 rows in set (0.00 sec)
 
 
 
@@ -109,5 +111,40 @@ DESC CONSULTA;
 */
 
 
+-- INSERINDO OS VALORES
+
+DESC MEDICO;
+-- Todos os valores de Nome,CRM e telefones são ficticios
+
+INSERT INTO MEDICO VALUES(1, 'Bruno Fraga', 'M', '784452', 'Endo', NULL);
+INSERT INTO MEDICO VALUES(2, 'Giovanna Lacerda', 'F', '158652', 'Psi', '99586254');
+INSERT INTO MEDICO VALUES(3, 'Goberto Arruda', 'M', '123548', 'Geral', '32546598');
+INSERT INTO MEDICO VALUES(4, 'Fernanda Gallo', 'F', '258654', 'Geral', NULL);
+INSERT INTO MEDICO VALUES(5, 'Bruna Mafra', 'F', '32546', 'Onco', '58426532');
+INSERT INTO MEDICO VALUES(6, 'Bruno Fraga', 'M', '784552', 'Endo', '75854582');
+INSERT INTO MEDICO VALUES(7, 'Felipe Vascos', 'M', '256357', 'Imuno', '78549652');
+INSERT INTO MEDICO VALUES(8, 'Bruna Vieira', 'F', '12548', 'Neuro', '25874563');
+INSERT INTO MEDICO VALUES(9, 'Matheus Alves', 'M', '12586', 'Neuro', NULL);
+INSERT INTO MEDICO VALUES(10, 'Felipe Gonçlves', 'M', '258365', 'Neuro', NULL);
+INSERT INTO MEDICO VALUES(11, 'Gabriella Ferreira Alves', 'F', '125484', 'Orto', '75854582');
+INSERT INTO MEDICO VALUES(12, 'Renam Gonçalves', 'M', '225487', 'Orto', '75854582');
+INSERT INTO MEDICO VALUES(13, 'Lucas Santos', 'M', '225484', 'Orto', NULL);
+INSERT INTO MEDICO VALUES(14, 'Letícia Ferraz', 'F', '234555', 'Psi', '75854582');
+INSERT INTO MEDICO VALUES(15, 'Vitória Alves', 'F', '125486', 'Geral', NULL);
+
+-- SELECTS : MEDICO 
+
+-- Retorna o nome do médico, o CRM e sua Especialidade médica.
+SELECT IDMEDICO AS "Matrícula",
+	   MEDICO AS "MÉDICO",
+	   CRM AS "REGISTRO" , 
+       ESPECIALIDADE AS "ESPECIALIDADE MÉDICA"
+FROM MEDICO;
+
+
+-- Retorna no momento 8 médicos e 7 médicas
+SELECT SEXO, COUNT(*) AS "Quantidade"
+FROM MEDICO
+GROUP BY 1;
 
 
